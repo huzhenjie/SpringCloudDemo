997cee37dd1a988093f5442b545d7a34970ab1f1
huzhenjie <huzhenjie.dev@gmail.com>
2021-05-23 22:55:54 +0800

    Add env test

--- background/src/test/java/com/scrat/background/BackgroundApplicationTests.java
+++ background/src/test/java/com/scrat/background/BackgroundApplicationTests.java
@@ -1,13 +1,23 @@
 package com.scrat.background;
 
+import com.scrat.background.module.env.EnvConfig;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackgroundApplicationTests {
 
+    private static final Logger log = LoggerFactory.getLogger(BackgroundApplicationTests.class.getName());
+
+    @Autowired
+    private EnvConfig envConfig;
+
     @Test
     void contextLoads() {
+        log.info(envConfig.getBuildDescription());
     }
 
 }
