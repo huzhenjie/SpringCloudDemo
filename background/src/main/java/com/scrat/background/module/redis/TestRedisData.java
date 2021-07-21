7b4d76388af1ccb2ff576df6dd60e4777fd62706
huzhenjie <huzhenjie.dev@gmail.com>
2021-07-21 08:39:11 +0800

    Redis Serializer

--- background/src/main/java/com/scrat/background/module/redis/TestRedisData.java
+++ background/src/main/java/com/scrat/background/module/redis/TestRedisData.java
@@ -1,9 +1,25 @@
 package com.scrat.background.module.redis;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class TestRedisData {
     private long id;
     private String name;
     private Integer num;
+    private String nullVal;
+    private String moreProperties;
+
+    private static final String PRIMARY_STATIC_PROPERTY = "primary_static_property";
+    public static final String PUBLIC_STATIC_PROPERTY = "public_static_property";
+
+    public String publicMethod() {
+        return "publicMethod";
+    }
+
+    @JsonIgnore
+    public String publicIgnoreMethod() {
+        return "publicIgnoreMethod";
+    }
 
     public long getId() {
         return id;
@@ -32,12 +48,32 @@ public class TestRedisData {
         return this;
     }
 
+    public String getNullVal() {
+        return nullVal;
+    }
+
+    public TestRedisData setNullVal(String nullVal) {
+        this.nullVal = nullVal;
+        return this;
+    }
+
+    public String getMoreProperties() {
+        return moreProperties;
+    }
+
+    public TestRedisData setMoreProperties(String moreProperties) {
+        this.moreProperties = moreProperties;
+        return this;
+    }
+
     @Override
     public String toString() {
         return "TestRedisData{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", num=" + num +
+                ", nullVal='" + nullVal + '\'' +
+                ", moreProperties='" + moreProperties + '\'' +
                 '}';
     }
 }
